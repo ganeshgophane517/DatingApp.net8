5c333dc56bac71a48052e95ebdaeb678f2092155
NoobAnna <05nairhari@gmail.com>
2024-11-20 15:48:10 +0530

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs;
 
 public class RegisterDto
 {
-    public required string username { get; set; }
-    public required string password{ get;set;}
+    [Required]
+    public  string username { get; set; } =string.Empty;
+    [Required]
+    [StringLength(8, MinimumLength =4)]
+    public string password{ get;set;} =string.Empty;
 
 }
